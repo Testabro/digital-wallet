12f478d739309978ae74519bf321c96eca1f09e8
Thomas Estabrook <thomas.r.estabrook@gmail.com>
2022-09-07 18:26:51 -0400

    admin: optimize include

--- src/Command.hpp
+++ src/Command.hpp
@@ -1,5 +1,9 @@
+#pragma once
 #include <string>
-#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
 #include <boost/random.hpp>
 
 class Command {
@@ -8,6 +12,10 @@ class Command {
         Command() = default;
         Command(std::string account1, std::string account2, std::string amount, std::string action) :
            _account1(account1), _account2(account2), _amount(amount), _action(action){ _id=generateID(); }
+        friend std::ostream& operator<<(std::ostream& os, const Command command) {
+           return os << command._id << " " << command._account1 << " " << command._account2 
+              << " " << command._amount << " " << command._action;
+        }
 
         std::string getID() { return _id; }; //DEBUG
         std::string getAction() { return _action; };
@@ -26,5 +34,16 @@ class Command {
         std::string _account1;
         std::string _account2;
         std::string _amount;
-        std::string _action; 
+        std::string _action;
+
+        friend class boost::serialization::access;
+        template<class Archive>
+        void serialize(Archive &ar,
+        const unsigned int version){
+            ar & _id;
+            ar & _account1;
+            ar & _account2;
+            ar & _amount;
+            ar & _action;
+        }
 };
