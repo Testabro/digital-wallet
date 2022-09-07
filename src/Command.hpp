926c607558b174b60a6341f30c020e95eb305f91
Thomas Estabrook <thomas.r.estabrook@gmail.com>
2022-09-07 15:20:17 -0400

    refactor: reduce 128-bit command id to 64-bit. Defect in 128 generation anyhow

--- src/Command.hpp
+++ src/Command.hpp
@@ -1,16 +1,14 @@
 #include <string>
-#include <boost/multiprecision/cpp_int.hpp>
+#include <ctime>
+#include <boost/random.hpp>
 
 class Command {
     //action to be performed to change the state of the system;
     public:
-        Command(std::string account1, std::string account2, std::string amount, std::string action) {
-             _account1 = account1;
-             _account2 = account2;
-             _amount = amount;
-             _action = action;
-             _id = generateID(); //DEBUG
-        };
+        Command() = default;
+        Command(std::string account1, std::string account2, std::string amount, std::string action) :
+           _account1(account1), _account2(account2), _amount(amount), _action(action){ _id=generateID(); }
+
         std::string getID() { return _id; }; //DEBUG
         std::string getAction() { return _action; };
         std::string getAccount1() { return _account1; };
@@ -18,16 +16,15 @@ class Command {
         std::string getAmount() { return _amount; };
         // Generate random 128-bit id; TODO create id manager process and delagate this
         std::string generateID() {
-            boost::multiprecision::int128_t int64left = CreateRandomInt64ViaBoost()
-            boost::multiprecision::int128_t int64right = CreateRandomInt64ViaBoost()
-            boost::multiprecision::int128_t randomInt = int64left << 64 | int64right;
-            return std::to_string(randomInt);
+            std::time_t now = std::time(0);
+            boost::random::mt19937 gen{static_cast<std::uint32_t>(now)};
+            return std::to_string(gen());
         };
 
     private:
+        std::string _id;
         std::string _account1;
         std::string _account2;
         std::string _amount;
-        std::string _action;
-        std::string _id;
+        std::string _action; 
 };
