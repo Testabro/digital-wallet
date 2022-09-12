0a88dc911d2686a72e515230792e5879685acf9e
Thomas Estabrook <thomas.r.estabrook@gmail.com>
2022-09-12 15:24:06 -0400

    admin: clean up

--- src/Service.h
+++ src/Service.h
@@ -20,12 +20,11 @@ class Service {
         void toggle();
         // This will get called by the current state
 	    void setState(ServiceState& newState);
-        // std::map<std::string, std::string> *_state_map;
+
         rocksdb::DB* _accountDB; //Init a rockdb handle for database access
         rocksdb::Status _status;
         rocksdb::ReadOptions _read_options;
         rocksdb::Options _options; 
-        // std::queue<Event> *_event_queue;
         std::queue<Command> *_command_queue;                
         ServiceState *currentState;        
     private:
