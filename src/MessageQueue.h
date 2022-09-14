e8734cdcdfd9a5eee7bba350dbf75167b2acca56
Thomas Estabrook <thomas.r.estabrook@gmail.com>
2022-09-14 12:20:30 -0400

    admin: clean up

--- src/MessageQueue.h
+++ src/MessageQueue.h
@@ -24,22 +24,13 @@ public:
 
     void send(T msg)
     {
-        // simulate some work
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
         // perform vector modification under the lock
         std::lock_guard<std::mutex> uLock(_mutex);
 
         // add vector to queue
         std::cout << "   Message " << msg << " has been sent to the queue" << std::endl;
         _messages.push_back(std::move(msg));
-        _cond.notify_one(); // notify client after pushing new Vehicle into vector
-    }
-
-    void waitForMessage() {
-        // perform queue modification under the lock
-        std::unique_lock<std::mutex> uLock(_mutex);
-        _cond.wait(uLock, [this] { return !_messages.empty(); }); // pass unique lock to condition variable        
+        _cond.notify_one(); // notify client after pushing new Command into vector
     }
 
 private:
