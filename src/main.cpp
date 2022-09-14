9ab6630edc1525f5668cee9dc372b50335d6ad0a
Thomas Estabrook <thomas.r.estabrook@gmail.com>
2022-09-14 07:24:01 -0400

    feature: generate account number

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 #include <sstream>        // osstringstream
 #include <assert.h>       //for test case
 #include <string>         //str()
+
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
+#include <boost/filesystem.hpp>
 
 #include "Service.h"
 #include "../lib/crowapi/crow_all.h"
@@ -14,47 +16,49 @@
 #include <future>
 #include <mutex>
 
+void generateSeedFile( std::string seed_file ) {
+    if ( !boost::filesystem::exists( seed_file ) ) {
+        int init_num = 0;
+        //Add to event log
+        {
+            std::ofstream ofs( seed_file );
+            boost::archive::text_oarchive oa(ofs);
+            oa << init_num;
+        }
+    }
+}
+
+std::string genAccountID( std::string seed_file ) {
+    //Init account generation seed file if it does not exist
+    generateSeedFile( seed_file );
+
+    int account_num = 0;
+    {
+        std::ifstream ifs(seed_file);
+        boost::archive::text_iarchive ia(ifs);
+        ia >> account_num;
+    }
+    
+    account_num++;
+    //Add to event log
+    {
+        std::ofstream ofs(seed_file);
+        boost::archive::text_oarchive oa(ofs);
+        oa << account_num;
+    }     
+
+    std::ostringstream os;
+
+    os << "7" << std::setw(8) << std::setfill('0') << account_num;
+    std::cout << os.str();
+    return os.str();
+}
 
 int main()
 {
     //Init API to process commands
     crow::SimpleApp app;
 
-    // TEST EVENT LOGGING W/ MEM MAPPED FILE
-    // Event event = Event();
-    // event.action = "test_action";
-    // event.account = "test_account";
-    // event.amount = "test_amount";
-    // event.parent_cmd_id = "test_parent_cmd_id";
-
-    // std::cout << event << std::endl;
-    // {
-    //     // std::ofstream ofs(FILENAME, ios::app);
-    //     // boost::archive::text_oarchive oa(ofs);
-    //     // oa << number_of_objects;
-    //     // for (size_t i = 0; i != number_of_objects; ++i) {
-    //     //     oa << data[i];
-    //     // }
-    //     std::ofstream ofs("/tmp/event-log.bin");
-    //     boost::archive::binary_oarchive oa(ofs);
-    //     oa << event;
-    // }
-    // Event new_event;
-    // std::ifstream ifs("/tmp/event-log.bin");
-    // {
-    //     // std::ifstream ifs(FILENAME);
-    //     // boost::archive::text_iarchive ia(ifs);
-    //     // size_t number_of_objects;
-    //     // ia >> number_of_objects;
-    //     // // allocate number_of_objects objects
-    //     // for (size_t i = 0; i != number_of_objects; ++i){ 
-    //     //     ia >> data[i];
-    //     // }
-    //     boost::archive::binary_iarchive ia(ifs);
-    //     ia >> new_event;
-    // }
-    // std::cout << new_event << std::endl;
-  
     //Init Service
     Service service = Service();
 
@@ -70,6 +74,31 @@ int main()
     service._status = service._accountDB->Put(rocksdb::WriteOptions(), key2, value2);
     assert(service._status.ok());
    
+    CROW_ROUTE(app, "/api/1.0/wallet/create_account")
+    ([&service](const crow::request& req) {
+        std::ostringstream os;
+
+        // To get a simple string from the url params
+        // To see it in action /params?foo='blabla'
+        os << "Params: " << req.url_params << "\n\n"; 
+        os << "The key 'startAmount' was " << (req.url_params.get("startAmount") == nullptr ? "not " : "") << "found.\n";
+        if ( req.url_params.get("startAmount") == nullptr ){
+            return crow::response{os.str()};
+        }
+        
+        std::string accountID = genAccountID("/tmp/account-seed.dat");
+        std::cout << accountID;
+        rocksdb::Slice key = accountID;
+        std::string value = req.url_params.get("startAmount");
+
+        // TEST modify the database
+        service._status = service._accountDB->Put(rocksdb::WriteOptions(), key, value);
+        assert(service._status.ok());
+
+        os << "Account: " << accountID << " : Balance: " << value << std::endl;
+
+        return crow::response{os.str()};
+    });
 
     //TEST BALENCE XFER LOGIC- Create a command object then add it to the command file
     CROW_ROUTE(app, "/api/1.0/wallet/balance_transfer")
@@ -106,21 +135,25 @@ int main()
     });
 
     // /api/1.0/wallet/get_balance
-    CROW_ROUTE(app, "/api/1.0/wallet/get_balance")([&service](){
+    CROW_ROUTE(app, "/api/1.0/wallet/get_balance")([&service](const crow::request& req){
         std::ostringstream os;
         std::string value;
 
+                // To get a simple string from the url params
+        // To see it in action /params?foo='blabla'
+        os << "Params: " << req.url_params << "\n\n"; 
+        os << "The key 'accountID' was " << (req.url_params.get("accountID") == nullptr ? "not " : "") << "found.\n";
+         if ( req.url_params.get("accountID") == nullptr ){
+            return crow::response{os.str()};
+        }
+        std::string accountID = req.url_params.get("accountID");
+        
         // TEST Get value account 1
-        service._status = service._accountDB->Get(service._read_options, "accountA", &value);
+        service._status = service._accountDB->Get(service._read_options, accountID, &value);
         assert(service._status.ok()); 
-        os << "Account 1: " << value << std::endl;
+        os << "Account: " << accountID << " : Balance: " << value << std::endl;
 
-        // // TEST Get value account 2
-        service._status = service._accountDB->Get(service._read_options, "accountB", &value);
-        assert(service._status.ok()); 
-        os << "Account 2: " << value << std::endl;
-
-        return os.str();
+        return crow::response{os.str()};
     });
 
         CROW_ROUTE(app, "/api/1.0/wallet/events")([](){
